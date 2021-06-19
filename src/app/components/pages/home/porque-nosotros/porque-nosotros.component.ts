b1162288d6bcf54b101c4e9bd27eba98c0d9662c
Camilo Berrio <camiloberriouribe@gmail.com>
2021-06-19 11:53:47 -0500

    Se agregan imagenes al home

--- src/app/components/pages/home/porque-nosotros/porque-nosotros.component.ts
+++ src/app/components/pages/home/porque-nosotros/porque-nosotros.component.ts
@@ -8,18 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class PorqueNosotrosComponent implements OnInit {
   whychooseus = [
     {
-      icon: 'fal fa-laptop-code',
-      title: 'Modify Whole System',
+      icon: 'fal fa-bolt',
+      title: 'Solución rápida',
       text: 'Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudan-tium totam rem aperiam eaque ipsa',
     },
     {
-      icon: 'fal fa-server',
-      title: 'Beneficial Strategies',
+      icon: 'fal  fa-balance-scale',
+      title: 'Expertos en derecho',
       text: 'Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudan-tium totam rem aperiam eaque ipsa',
     },
     {
-      icon: 'fal fa-tools',
-      title: 'Automated Software',
+      icon: 'fal fa-user',
+      title: 'Constante contacto',
       text: 'Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudan-tium totam rem aperiam eaque ipsa',
     },
   ];
